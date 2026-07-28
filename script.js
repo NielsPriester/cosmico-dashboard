e979a2c13b63673bc6fcb156ffaaecb76b23dd07
NIels <niels@mangosbeachbar.nl>
2026-07-28 17:55:03 +0200

    Eerste Dashboard

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("app").innerHTML = `
+<h1 style="padding:40px;font-size:60px;">
+COSMICO BEACH
+</h1>
+
+<p style="padding:40px;font-size:30px;">
+Dashboard succesvol gestart 🚀
+</p>
+`;
