66af4bb8dc55d537af2306e8bea219bf2c34450e
NIels <niels@mangosbeachbar.nl>
2026-07-28 18:08:25 +0200

    Live weer dashboard v1

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById("app").innerHTML = `
-<h1 style="padding:40px;font-size:60px;">
-COSMICO BEACH
-</h1>
-
-<p style="padding:40px;font-size:30px;">
-Dashboard succesvol gestart 🚀
-</p>
-`;
+// JavaScript staat voorlopig in index.html
